7a9a8e49c7ce923c5fc68851f4bdf9349503288a
benholtzmansmith <benjamin.holtzman.smith@gmail.com>
2016-11-18 18:44:04 -0500

    adding more mem and website

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-npm start & spark-submit --master local[*] --conf spark.driver.memory=512m --class trump.twitter.Stream /app/trump-twitter-stream-assembly-0.0.1.jar /app/model >/dev/null
+npm start & spark-submit --master local[*] --conf spark.driver.memory=4G --class trump.twitter.Stream /app/trump-twitter-stream-assembly-0.0.1.jar /app/model >/dev/null
 
